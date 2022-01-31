43ec806fb6f3106f6cc490d95ac86a32861eda34
pchlenski <pac@cs.columbia.edu>
2022-01-31 17:23:29 -0500

    Added dist functions to _init_full

--- src/OmicsGenerator.py
+++ src/OmicsGenerator.py
@@ -1105,10 +1105,13 @@ class OmicsGenerator:
                 j += node2.size
             i += node1.size
 
+    def _random(size) -> np.ndarray:
+        return 2 * (0.5 - np.random.rand(size=size))
+
     def _init_full(
         self,
         initial_distribution : callable = np.random.exponential,
-        growth_rate_distribution : callable = 2 * (0.5 - np.random.rand),
+        growth_rate_distribution : callable = self._random,
         **kwargs) -> None:
         """
         A fully random initialization of all generator parameters.

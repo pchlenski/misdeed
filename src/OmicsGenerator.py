04ae5243edc3000d51402ee1a015fdc60d414b42
Philippe Chlenski <p@chlenski.com>
2022-02-15 19:16:10 -0500

    Bug

--- src/OmicsGenerator.py
+++ src/OmicsGenerator.py
@@ -1208,7 +1208,7 @@ class OmicsGenerator:
         node_size = self.get(name=node_name).size
 
         # Get response vector
-        if istype(callable, response_distribution):
+        if isinstance(callable, response_distribution):
             vector = response_distribution()
         elif response_distribution is None:
             vector = effect_size * (0.5 - np.random.rand(node_size))
